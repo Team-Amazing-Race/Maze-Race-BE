0400e502a731ff6fd1b4c959a5ed953d909bcaac
Sam Jespersen <heyheysamj@gmail.com>
2019-12-05 15:13:36 -0800

    sockets are working!

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-// loads the .env environment variables
 require('dotenv').config();
 
-// require the app http event handler
 const app = require('./lib/app');
-// create an http server that uses app
-const { createServer } = require('http');
-const server = createServer(app);
 
-// start the server by listening on a port
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Started on ${PORT}`);
 });
